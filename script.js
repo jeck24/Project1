98fe4f1922fac18cb7cd9427e870616afa93719e
JC <sanjothebay@gmail.com>
2020-11-06 21:22:25 -0600

    ajax call

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-
-$.ajax({
-    headers: { 'X-Auth-Token': '9bcb297f76e444a6926ba9eae377962b' },
-    url: "https://api.football-data.org/v2/competitions/PL/matches?matchday=11",
-    dataType: 'json',
-    type: 'GET',
-}).done(function(response) {
+let usersInput;
 
 
-// do something with the response, e.g. isolate the id of a linked resource   
-    console.log(response);
+function searchSoccerTeam(usersInput) {
+
+   const settings = {
+   	   "async": true,
+   	   "crossDomain": true,
+   	   "url": "https://api-football-v1.p.rapidapi.com/v2/teams/search/" + usersInput,
+   	   "method": "GET",
+   	   "headers": {
+   		   "x-rapidapi-key": "b8ca7b92b3mshba41efe52c0ecb1p1a174cjsnb6d350ce8924",
+   		   "x-rapidapi-host": "api-football-v1.p.rapidapi.com"
+   	}
+   };
+   
+   $.ajax(settings).done(function (response) {
+   	console.log(response);
+   
+
+       $("#firstCardHolderImg").append("<img>" + response.api.teams[0].logo + "</img>");
+
+
+
+
+
+
+
+
+
+
+});
+
+}
+
+
+
+$("button").on("click", (event) => {
+    event.preventDefault();
+       console.log("click")
+       console.log(usersInput)
+
+       usersTeamInputSearch = $(".searchBar").val().trim();
+
+       searchSoccerTeam(usersTeamInputSearch)
+
+
 });
